a98cfd731bb628474c0a9b0f952974695a65148c
Ross Clay <clay.rossclay@gmail.com>
2024-07-06 07:07:49 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname + "/dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Webpack Template',
+            filename: 'index.html',
+            inject: 'body',
+        })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+};
