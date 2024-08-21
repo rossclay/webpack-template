b833123de72d0e77d6850c3a7212a2bdf6317282
Ross Clay <clay.rossclay@gmail.com>
2024-08-21 11:18:32 -0400

    includes eval source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-const path = require('path')
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: "./src/index.js",
-    devtool: 'inline-source-map',
-    output: {
-        path: path.resolve(__dirname + "/dist"),
-        filename: "bundle.js",
-        clean: true,
-    },
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            title: 'Webpack Template',
-            filename: 'index.html',
-            inject: 'body',
-        })],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Webpack Template",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
